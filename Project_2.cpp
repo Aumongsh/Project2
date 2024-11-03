88ab24f4158fd2d53505ed2cf1597f46b142bcb4
MOMOKA AUNG <mshibuy388@student.glendale.edu>
2024-11-02 20:47:13 -0700

    Added some changes.

--- Project_2.cpp
+++ Project_2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -16,8 +18,8 @@ void fillArray(vector<vector<CellType>>& grid, int threshold_wall)
 	{
 		for (int j = 0; j < SIZE; j++)
 		{
-			int randomnum = rand() % 100; //assign ramdom number 0-100. If the number is greater than 50 then it becomes a wall cell.
-			if (randomnum > threshold_wall)
+			int randomnum = rand() % 100; //assign ramdom number 0-100. 
+			if (randomnum > threshold_wall) //if the number is greater than threshold_wall then it becomes a wall cell.
 			{
 				grid[i][j] = WALL; //set as wall
 			}
@@ -27,25 +29,29 @@ void fillArray(vector<vector<CellType>>& grid, int threshold_wall)
 			}
 		}
 	}
+	grid[0][0] = START;
+    grid[SIZE - 1][SIZE - 1] = END;
 }
 
-//possible move: down, right, up, left
+//possible move: up, right, down, left
 int adjacentRows[] = { -1, 0 , 1, 0 };
 int adjacentColumns[] = { 0, 1, 0, -1 };
 
-//check a cell if it is valid to go forword
+//check a cell if its in bound
 bool isValid( int rows, int cols)
 {
 	return (rows >= 0 && rows < SIZE && cols >= 0 && cols < SIZE);	 
 }
 
 int bfs_search(vector<vector<CellType>>& grid, pair<int, int> START,
- pair<int, int> END, vector<vector<bool>>& visited)
+ pair<int, int> END)
 {
 	queue <pair <int, int>> que;
+	vector<vector<bool>> visited(SIZE, vector<bool>(SIZE, false));
+	vector<vector<int>> distance(SIZE, vector<int>(SIZE, -1));
+
 	que.push(START);
 	visited[START.first][START.second] = true;
-	vector<vector<int>> distance(SIZE, vector<int>(SIZE, -1));
 	distance[START.first][START.second] = 0;
 
 	while (!que.empty())
@@ -76,19 +82,33 @@ int bfs_search(vector<vector<CellType>>& grid, pair<int, int> START,
 	return -1; //not an available path was found
 }
 
+void printGrid(const vector<vector<CellType>>& grid) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            char symbol = (grid[i][j] == WALL) ? '#' :
+			(grid[i][j] == EMPTY) ? '.' :
+			(grid[i][j] == START) ? 'S' : 'E';
+            cout << symbol << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
+	srand(static_cast<unsigned int>(time(0)));
 	vector<vector<CellType>> grid(SIZE, vector<CellType>(SIZE, EMPTY));
-	vector<vector<bool>> visited(SIZE, vector<bool>(SIZE, false)); 
 
 	fillArray(grid, 90);
+
+	printGrid(grid);
 
 	pair<int, int> START = { 0, 0 };
 	pair<int, int> END = { SIZE - 1, SIZE - 1 };
 
-	int shortest_distances = bfs_search(grid, START, END, visited);
+	int shortest_distances = bfs_search(grid, START, END);
 
-	if (shortest_distances == true)
+	if (shortest_distances != -1)
 	{
 		cout << "Found the shortest route using BFS algorithm." << endl;
 		cout << "Total path length: " << shortest_distances << endl;
